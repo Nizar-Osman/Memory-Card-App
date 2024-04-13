270384850678a06d865b84c1de7804aec896e2c1
Nizar-Osman <117791265+Nizar-Osman@users.noreply.github.com>
2024-04-12 20:03:35 -0400

    Added Card.jsx - rendered to App.jsx

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(props) {
+    return (
+        <div className="card">
+            <h2>Name</h2>
+            <img src="https://via.placeholder.com/100" alt="Meaning" />
+            <p>Meaning</p>
+
+        </div>
+    );
+} 
+
+export default Card;
