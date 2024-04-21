4be7d7a9ec5e43c539b96db762fa285f7af950c9
Nizar-Osman <117791265+Nizar-Osman@users.noreply.github.com>
2024-04-20 20:39:27 -0400

    Add file SideA.jsx

---
+++ src/SideA.jsx
@@ -0,0 +1,12 @@
+
+
+function SideA(props) {
+    return (
+        <div className="card">
+            <h2>{props.name}</h2>
+            <p>{props.meaning}</p>
+        </div>
+    );
+} 
+
+export default SideA;
